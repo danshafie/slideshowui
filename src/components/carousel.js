0778b8d388243060cfd42c70f11c93234677aeab
Dan Shafie <danshafie@gmail.com>
2016-11-13 15:38:23 -0800

    added slideshow css

--- src/components/carousel.js
+++ src/components/carousel.js
@@ -6,27 +6,52 @@ import { getScores } from '../actions/index';
 
 class Carousel extends Component {
 
+
   componentWillMount() {
     this.props.getScores();
   }
 
-  renderGames() {
-    return this.props.scores.map(game => {
-      console.log('game: ', game);
-      return (
-        <div className='thumbnails'>
-          <h3>{game.away_team_city} @ {game.home_team_city}</h3>
-          <img src={game.video_thumbnails.thumbnail[0].content} />
-        </div>
-      )
-    })
-  }
+  // renderGames() {
+  //   return this.props.scores.map(game => {
+  //     console.log('game: ', game);
+  //     return (
+  //       <div className='thumbnails'>
+  //         <h3>{game.away_team_city} @ {game.home_team_city}</h3>
+  //         <img src={game.video_thumbnails.thumbnail[0].content} />
+  //       </div>
+  //     )
+  //   })
+  // }
 
   render() {
     console.log('this is state in carousel: ', this.props.scores);
     return (
-      <div className='carousel'>
-        {this.renderGames()}
+      <div className='contain'>
+        <div className='row'>
+          <div className='row__inner'>
+        {this.props.scores.map(game => {
+          return (
+            <div className='tile'>
+              <div className='tile__details'>
+                <div className='tile_title'>                  
+                  {game.game_media.media[0].title}
+                </div>
+              </div>
+              <div className='tile__media'>
+                <img className='tile__img' src={game.video_thumbnails.thumbnail[0].content} />
+              </div>
+            </div>
+
+            // <div className='thumbnails'>
+            //   <h3>{game.away_team_city} @ {game.home_team_city}</h3>
+            //   <div className='img-div'>
+            //     <img src={game.video_thumbnails.thumbnail[0].content} />
+            //   </div>
+            // </div>
+          )
+        })}
+          </div>
+        </div>
       </div>
     )
   }
